af3606edd6d4a23c0c2bba071043227491578ec7
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-01-11 13:25:16 +0530

    Day 11: 2099. Find the subsequence of length k with the largest sum

---
+++ Week_2( 8th - 15th_Jan_24)/Subsequence.py
@@ -0,0 +1,16 @@
+#2099. Find the subsequence of length k with the largest sum
+
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        pq = []
+        heapq.heapify(pq)
+        for i in range (len(nums)):
+            heapq.heappush(pq,[nums[i], i])
+            if ( len(pq) > k ):
+                heapq.heappop(pq)
+        
+        pq.sort( key = lambda x : x[1] )
+        ans = []
+        for i in range (len(pq)):
+            ans.append(pq[i][0])
+        return ans 
